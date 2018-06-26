217dd5640b68531fa10ab77dedb483ad11b680ad
João Müller <34399432+jvmuller@users.noreply.github.com>
2018-06-26 02:17:42 -0300

    ALTv2.0
    Essa ideia não é algo fixo, a principio será continuado o desenvolvimento do projeto principal v1.
    
    + Novo conceito para desenvolvimento
    ---> O jogador não precisa fazer cadastro, terá apenas um nome de usuário para cadastrar sua pontuação no ranking.
    ---> Será utilizado bootstrap em vez de materialize pela devasta quantia de recursos de interesse para o site.
    + Nova estrutura de funcionamento
    ---> O site será baseado em uma página única, contendo o menu com a logotipo do QuestionUP, uma aba Home e outra para aba Ranking.
    ---> Na página inicial irá possuir um pequeno formulário centralizado pedindo um nome de usuário e logo abaixo terá três opção, sendo o botão jogar (possivelmente terá opção para escolha de categoria de perguntas), ranking e instruções.

--- forms.py
+++ forms.py
@@ -1,49 +1,26 @@
 from flask_wtf import FlaskForm as BaseForm
-from wtforms import TextField, PasswordField, SelectField, StringField
-from wtforms.validators import DataRequired, Length, EqualTo
+from wtforms import StringField, SelectField
+from wtforms.validators import DataRequired, Lenght
 
-class RegistrarForm(BaseForm):
-	nome = StringField(
-		'nome',
-		validators=[DataRequired()]
-	)
-	usuario = StringField(
-		'usario',
-		validators=[DataRequired(), Length(min=3, max=25)]
-	)
-	senha = PasswordField(
-		'senha',
-		validators=[DataRequired(), Length(min=3, max=25)]
-	)
-	confirmacao = PasswordField(
-		'confirmacao',
-		validators=[DataRequired(), EqualTo('senha', message='Senhas precisam ser iguais.')]
-	)
+class FormLogin(BaseForm):
+    usuario = StringField('usuario', validators=[DataRequired()])
 
-class EntrarForm(BaseForm):
-	usuario = StringField('usuario', validators=[DataRequired()])
-	senha = PasswordField('senha', validators=[DataRequired()])
+class FormEnvio(BaseForm):
+    questao = StringField('questao', validators=[DataRequired()])
+    opcao1 = StringField('opcao1', validators=[DataRequired()])
+    opcao2 = StringField('opcao2', validators=[DataRequired()])
+    opcao3 = StringField('opcao3', validators=[DataRequired()])
+    opcao4 = StringField('opcao4', validators=[DataRequired()])
+    opcao5 = StringField('opcao5', validators=[DataRequired()])
+    resposta = SelectField(
+        'resposta',
+        choices=[('enem','ENEM')],
+        validators=[DataRequired()]
+    )
 
-class EnviarForm(BaseForm):
-	questao = StringField('questao', validators=[DataRequired()])
-	opcao1 = StringField('opcao1', validators=[DataRequired()])
-	opcao2 = StringField('opcao2', validators=[DataRequired()])
-	opcao3 = StringField('opcao3', validators=[DataRequired()])
-	opcao4 = StringField('opcao4', validators=[DataRequired()])
-	resposta = SelectField(
-		'resposta',
-		choices=[('opcao1', 'A'), ('opcao2', 'B'), ('opcao3', 'C'), ('opcao4', 'D')],
-		validators=[DataRequired()]
-	)
-	categoria = SelectField(
-		'categoria',
-		choices=[('enem', 'ENEM'), ('cfoav','CFOAV'), ('detran','DETRAN')],
-		validators=[DataRequired()]
-	)
-
-class QuizForm(BaseForm):
-	resposta_escolhida = SelectField(
-		'resposta_escolhida',
-		choices=[('opcao1', 'A'), ('opcao2', 'B'), ('opcao3', 'C'), ('opcao4', 'D')],
-		validators=[DataRequired()]
-	)
+class FormJogo(BaseForm):
+    tentativa_resposta = SelectField(
+        'tentativa_resposta',
+        choices=[('opcao1','A'),('opcao2','B'),('opcao3','C'),('opcao4','D'),('opcao5','E')],
+        validators=[DataRequired()]
+    )
